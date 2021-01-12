344c76e876895cea8aaed6aeec77d8051d371a91
M.J.Abdullah <devopfrontend222@gmail.com>
2021-01-12 11:25:10 +0530

    Finalized version

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+const passwordDisplay = document.querySelector('.password');
+const copyIcon = document.querySelector('.fa-copy');
+const btn = document.querySelector('.btn');
+const copiedMsg = document.querySelector('.copied-msg');
+
+let completeLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]{}^*,./()';
+let letters = completeLetters.split('');
+let password = '';
+
+btn.addEventListener('click', () => {
+
+    password = '';
+
+    for (let count = 0; count < 16; count++) {
+        password += letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    passwordDisplay.value = password;
+
+});
+
+
+copyIcon.addEventListener('click', () => {
+
+    if (passwordDisplay.value.length >= 16) {
+        passwordDisplay.select();
+        if (document.execCommand('copy')) {
+            passwordDisplay.focus();
+            
+            copiedMsg.style.opacity = 1;
+            copiedMsg.style.marginLeft = '10px';
+            copiedMsg.style.pointerEvents = 'auto';
+
+            setTimeout(() => {
+                copiedMsg.style.opacity = 0;
+            }, 1200);
+            
+        }
+        //console.log('working');
+    }
+    else{
+        //console.log('notworking');
+        alert('Password is too short to copy!');
+    }
+});
